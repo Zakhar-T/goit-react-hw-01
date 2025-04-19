05220757d71ed629359d9ce97b7128c6f0bc89cf
Zakhar-T <zakhar.null@gmail.com>
2025-04-19 17:38:23 +0300

    complete tasks 1-3

---
+++ src/components/App/App.jsx
@@ -0,0 +1,25 @@
+import userData from '../Profile/userData.json';
+import friends from '../FriendList/friends.json';
+import transactions from '../TransactionHistory/transactionHistory.json';
+import { Profile } from '../Profile/Profile';
+import { FriendList } from '../FriendList/FriendList';
+import { TransactionHistory } from '../TransactionHistory/TransactionHistory';
+import css from './App.module.css';
+
+export default function App() {
+  return (
+    <div className={css.container}>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+      <div className={css.friendsContainer}>
+        <FriendList friends={friends} />
+      </div>
+      <TransactionHistory items={transactions} />
+    </div>
+  );
+}
---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,26 @@
+import clsx from 'clsx';
+import css from './FriendList.module.css';
+
+export function FriendList({ friends }) {
+  return friends.map((friend) => {
+    return (
+      <div className={css.container} key={friend.id}>
+        <img
+          className={css.friendAvatar}
+          src={friend.avatar}
+          alt="Avatar"
+          width="100"
+        />
+        <p className={css.friendName}>{friend.name}</p>
+        <p
+          className={clsx(
+            css.friendStatus,
+            friend.isOnline === true && css.friendOnline,
+          )}
+        >
+          {friend.isOnline === true ? 'Online' : 'Offline'}
+        </p>
+      </div>
+    );
+  });
+}
